9fd76c5226ea110c552cbb5f950f126a2e7d31c2
d-bernat <mail@it-bernat.de>
2020-10-15 16:14:13 +0200

    init

--- src/main/java/de/itbernat/springboot/demoboot/DemoBootApplication.java
+++ src/main/java/de/itbernat/springboot/demoboot/DemoBootApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package de.itbernat.springboot.demoboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoBootApplication.class, args);
 	}
 
 }
--- src/test/java/de/itbernat/springboot/demoboot/DemoBootApplicationTests.java
+++ src/test/java/de/itbernat/springboot/demoboot/DemoBootApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package de.itbernat.springboot.demoboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class DemoBootApplicationTests {
 
 	@Test
 	void contextLoads() {
